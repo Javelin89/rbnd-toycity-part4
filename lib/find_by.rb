02b31a152e65db5dd07fc4205121cfb4417d96a2
Javelin89 <chris.beermann89@gmail.com>
2016-03-10 00:05:42 +0100

    Fixed data path in find_by.rb

--- lib/find_by.rb
+++ lib/find_by.rb
@@ -4,7 +4,7 @@ class Module
     attributes.each do |attribute, location|
       find_by = %Q{
         def find_by_#{attribute}(argument)
-          products = CSV.read(File.dirname(__FILE__) + "/../data/data.csv")
+          products = CSV.read(File.dirname(__FILE__) + "/data/data.csv")
           products.each do |product|
             if #{location} == argument
               return Product.new(id: product[0], brand: product[1], name: product[2], price: product[3].to_f)
